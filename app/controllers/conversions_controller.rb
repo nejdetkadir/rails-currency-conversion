45817444a946c77232873aa20966cb7f69437407
nejdetkadir <nejdetkadir.550@gmail.com>
2021-12-12 02:16:54 +0300

    List all conversions and create destroying action for currencies controller

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,8 +1,9 @@
 # app/controllers/conversions_controller.rb
 class ConversionsController < ApplicationController
-  def index
-    @conversions = Conversion.page(params[:page])
-  end
+  before_action :get_conversations, only: [:new, :index, :create]
+  before_action :set_conversion, only: [:destroy]
+
+  def index; end
 
   def new
     @conversion = Conversion.new
@@ -13,15 +14,30 @@ class ConversionsController < ApplicationController
 
     if @conversion.save
       flash[:notice] = "Conversion was successfully created."
-      redirect_to root_path
+      redirect_to new_conversion_path
     else
       render :new
     end
+  end
+
+  def destroy
+    @conversion.destroy
+
+    flash[:notice] = "Conversion was successfully destroyed."
+    redirect_to new_conversion_path
   end
 
   private
 
   def conversion_params
     params.require(:conversion).permit(:currency_id, :to_id)
+  end
+
+  def get_conversations
+    @conversions = Conversion.page(params[:page])
+  end
+
+  def set_conversion
+    @conversion = Conversion.find(params[:id])
   end
 end
